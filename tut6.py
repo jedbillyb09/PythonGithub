95b0949a107b58be8df8c6fb3b8499fbbd4c3d94
jedbillyb <jedblenkhorn@gmail.com>
2025-03-19 21:15:35 +1300

    Added exercise 3 and # all lines

--- tut6.py
+++ tut6.py
@@ -1,4 +1,19 @@
-# Exercise 1
-list = ['rose','tulip','daisy']
-print(list)
-print(f"")
+# # Exercise 1
+# list = ['rose','tulip','daisy']
+# print(list)
+# print(f"")
+
+
+
+
+# # exercise 3
+# filling_list = []
+# loop_count = int(1)
+# print("You can have 3 fillings in your toasted sandwich")
+
+# while loop_count < 4:
+#     filling_name = input(f"What is filling {loop_count} for you toasty? ")
+#     filling_list.append(filling_name)
+#     loop_count = loop_count + 1
+
+# print(f"The order is {filling_list[0]}, {filling_list[1]}, {filling_list[2]}")
