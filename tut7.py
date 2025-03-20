2c6d7dc5212264e5baa0d2d4d751e10987179cc4
jedbillyb <jedblenkhorn@gmail.com>
2025-03-20 21:43:17 +1300

    finished tut 8

--- tut7.py
+++ tut7.py
@@ -14,10 +14,11 @@
 
 
 
-# # Excersie 2
+# Excersie 2
+
 # def tuple1 ():
 #     List_tuple = ("Jed", "Blenkhorn", 11)
-#     print(f"Your First name is {List_tuple[0]}, and your Last name is {List_tuple[1]}, and you are year {List_tuple[2]}.")
+#     print(f"Your first name is {List_tuple[0]}, and your last name is {List_tuple[1]}, and I am year {List_tuple[2]}.")
 # tuple1()
 
 
@@ -36,3 +37,6 @@
 # while list_count < len(name_age_list_tuple):
 #     print(f'The first name is {name_age_list_tuple[list_count][0]}, middle name {name_age_list_tuple[list_count][1]}, last name is {name_age_list_tuple[list_count][2]}, there age is {name_age_list_tuple[list_count][3]}, and there class name is {name_age_list_tuple[list_count][4]}')
 #     list_count += 1
+
+
+
---
+++ tut8.py
@@ -0,0 +1,14 @@
+# 1
+# print("hello world")
+
+
+
+cooking_dictionary = {"pizza": "oven", "bread": "toaster", "Roast Chicken": "oven", "Muffins": "toaster"}
+def sub1 ():
+        print(cooking_dictionary)
+        print('To cook pizza we use', cooking_dictionary["pizza"])
+        task_time = (f"To cook bread we use {cooking_dictionary['bread']}")
+        print(task_time)
+        print(f"To cook roast chicke we use {cooking_dictionary['Roast Chicken']}")
+        print(f"To cook muffins we use {cooking_dictionary['Muffins']}")
+sub1()
