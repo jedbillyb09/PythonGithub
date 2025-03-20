82ad9018af4b3b1fc0315722a28928ea4633de89
jedbillyb <jedblenkhorn@gmail.com>
2025-03-20 21:08:56 +1300

    finished tut 7

---
+++ tut7.py
@@ -0,0 +1,38 @@
+# def sub1 ():
+#     age = input("What is your name? ")
+#     name.append(age)
+
+# def sub2 ():
+#     print(f"Your name is {name[0]}")
+
+# name = []
+# sub1()
+# sub2()
+
+
+
+
+
+
+# # Excersie 2
+# def tuple1 ():
+#     List_tuple = ("Jed", "Blenkhorn", 11)
+#     print(f"Your First name is {List_tuple[0]}, and your Last name is {List_tuple[1]}, and you are year {List_tuple[2]}.")
+# tuple1()
+
+
+
+
+
+
+
+
+
+
+# Excercise 3
+
+# name_age_list_tuple = [("Jack","Robbert","Smith", 14,"10SMO"), ("Jim","Jimmy","Jackson", 16,"11BCA"), ("Jason","Rose","Mont", 15,"9GBA")]
+# list_count = 0
+# while list_count < len(name_age_list_tuple):
+#     print(f'The first name is {name_age_list_tuple[list_count][0]}, middle name {name_age_list_tuple[list_count][1]}, last name is {name_age_list_tuple[list_count][2]}, there age is {name_age_list_tuple[list_count][3]}, and there class name is {name_age_list_tuple[list_count][4]}')
+#     list_count += 1
